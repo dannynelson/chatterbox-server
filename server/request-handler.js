e11e58043a9fbd4f05fcdc95b5f7e6fcdd84ebb9
Daniel Nelson <danielnelsonguitar@gmail.com>
2013-12-07 11:04:36 -0800

    Handles POST and OPTIONS

--- server/request-handler.js
+++ server/request-handler.js
@@ -14,27 +14,44 @@ exports.handleRequest = function(request, response) {
 
   var statusCode;
   console.log("Serving request type " + request.method + " for url " + request.url);
-  if (request.url == "http://127.0.0.1:8080/classes/room1") {
+  // if (request.url == "http://127.0.0.1:8080/classes/room1") {
+    var headers = defaultCorsHeaders;
+
     if (request.method === "GET") {
       statusCode = 200;
+    }
+    if (request.method === 'OPTIONS') {
+      console.log('!OPTIONS');
+      headers = {};
+      // IE8 does not allow domains to be specified, just the *
+      headers["Access-Control-Allow-Origin"] = request.headers.origin;
+      headers["Access-Control-Allow-Origin"] = "*";
+      headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+      headers["Access-Control-Allow-Credentials"] = false;
+      headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+      headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+      // response.writeHead(200, headers);
+      // response.end();
     }
     if (request.method === "POST") {
       statusCode = 201;
       request.setEncoding('utf8');
       request.on('data', function(data) {
-        thisResponse.push(JSON.parse(data));
-        console.log(thisResponse);
+        data = JSON.parse(data);
+        data['createdAt'] = "2013-10-07T16:22:03.280Z";
+        data['updatedAt'] = '2013-10-07T16:22:03.280Z';
+        responseObj.results.push(data);
+        console.log(responseObj);
       });
     }
-  } else {
-    statusCode = 404;
-  }
+  // } else {
+  //   statusCode = 404;
+  // }
 
 
 
   /* Without this line, this server wouldn't work. See the note
    * below about CORS. */
-  var headers = defaultCorsHeaders;
   
   headers['Content-Type'] = "application/json";
 
@@ -49,11 +66,11 @@ exports.handleRequest = function(request, response) {
   //   thisResponse = responseObj;
   // } else 
 
-  console.log(thisResponse);
-  response.end(JSON.stringify(thisResponse));
+  // console.log(thisResponse);
+  response.end(JSON.stringify(responseObj));
 };
 
-var thisResponse = [];
+// var thisResponse = [];
 
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -69,21 +86,21 @@ var defaultCorsHeaders = {
 };
 
 var responseObj = {};
-// responseObj.results = [
-//   {
-//     createdAt: "2013-10-07T16:22:03.280Z",
-//     objectId: "teDOY3Rnpe",
-//     roomname: "lobby",
-//     text: "hello",
-//     updatedAt: "2013-10-07T16:22:03.280Z",
-//     username: "gary"
-//   }, 
-//   {
-//     createdAt: "2013-10-07T16:22:03.280Z",
-//     objectId: "teDOY3Rnpe",
-//     roomname: "lobby",
-//     text: "wassup",
-//     updatedAt: "2013-10-07T16:22:03.280Z",
-//     username: "gary"
-//   }
-// ];
+responseObj.results = [
+  {
+    createdAt: "2013-10-07T16:22:03.280Z",
+    objectId: "teDOY3Rnpe",
+    roomname: "lobby",
+    text: "hello",
+    updatedAt: "2013-10-07T16:22:03.280Z",
+    username: "gary"
+  },
+  {
+    createdAt: "2013-10-07T16:22:03.280Z",
+    objectId: "teDOY3Rnpe",
+    roomname: "lobby",
+    text: "wassup",
+    updatedAt: "2013-10-07T16:22:03.280Z",
+    username: "gary"
+  }
+];
